6db7f563ef79c9f8f751c3f0a3e525ea575363bf
sanjay <sanjaykumar73189@gmail.com>
2023-03-04 18:28:15 +0530

    formatting

---
+++ example/lib/Screens/Register.dart
@@ -0,0 +1,119 @@
+import 'package:chat_app_plugin/chat_app_plugin.dart';
+import 'package:chat_app_plugin/database_service.dart';
+import 'package:chat_app_plugin_example/Screens/Login.dart';
+import 'package:chat_app_plugin_example/User.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+  static String name = "";
+  static String email = "";
+  static String password = "";
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+Users users = new Users(dp: "", email: "", password: "", uid: "");
+
+class _RegisterState extends State<Register> {
+  final _chatAppPlugin = ChatAppPlugin();
+  final database = DatabaseService();
+
+  Future register(String name, String email, String password) async {
+    try {
+      await _chatAppPlugin
+          .withoutphotoregisterwithemailpassword(email, password, name)
+          .then((value) {
+        users.email = email;
+        users.password = password;
+
+        print(users.email);
+      });
+
+      // print(uid);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Center(
+              child: Padding(
+            padding: const EdgeInsets.all(38.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 28.0, bottom: 30),
+                  child: Text(
+                    'Register',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Text("Name"),
+                TextFormField(
+                  onChanged: (value) {
+                    Register.name = value;
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Text("Email"),
+                ),
+                TextFormField(
+                  onChanged: (value) {
+                    Register.email = value;
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Text("Password"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  onChanged: (value) {
+                    Register.password = value;
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        register(
+                            Register.name, Register.email, Register.password);
+                      },
+                      child: Text("Register")),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Login(),
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Login"),
+                  ),
+                )
+              ],
+            ),
+          )),
+        ),
+      ),
+    );
+  }
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:chat_app_plugin/database_service.dart';
+import 'package:chat_app_plugin_example/Screens/Login.dart';
+import 'package:chat_app_plugin_example/Screens/Register.dart';
 import 'package:chat_app_plugin_example/User.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -23,97 +25,16 @@ void main() async {
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
-  static String name = "";
-  static String email = "";
-  static String password = "";
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
-Users users = new Users(dp: "", email: "", password: "", uid: "");
-
 class _MyAppState extends State<MyApp> {
-  final _chatAppPlugin = ChatAppPlugin();
-  final database = DatabaseService();
-
-  Future register(String name, String email, String password) async {
-    try {
-      await _chatAppPlugin
-          .withoutphotoregisterwithemailpassword(email, password, name)
-          .then((value) {
-        users.email = email;
-        users.password = password;
-
-        print(users.email);
-      });
-
-      // print(uid);
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-            child: Padding(
-          padding: const EdgeInsets.all(38.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 28.0, bottom: 30),
-                child: Text(
-                  'Register',
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
-              Text("Name"),
-              TextFormField(
-                onChanged: (value) {
-                  MyApp.name = value;
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Text("Email"),
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  MyApp.email = value;
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Text("Password"),
-              ),
-              TextFormField(
-                obscureText: true,
-                onChanged: (value) {
-                  MyApp.password = value;
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: ElevatedButton(
-                    onPressed: () {
-                      register(MyApp.name, MyApp.email, MyApp.password);
-                    },
-                    child: Text("Register")),
-              )
-            ],
-          ),
-        )),
-      ),
+      home: Register(),
     );
   }
 }
