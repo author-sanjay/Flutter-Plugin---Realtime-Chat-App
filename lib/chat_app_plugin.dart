28bd3a2d95bc0b4a4f6e7f204d03272640c1761b
sanjay <sanjaykumar73189@gmail.com>
2023-03-04 20:16:19 +0530

    signin methods

--- lib/chat_app_plugin.dart
+++ lib/chat_app_plugin.dart
@@ -52,6 +52,28 @@ class ChatAppPlugin {
     return false;
   }
 
+  Future<String?> tokenwithphotoregisterwithemailpassword(
+      String email, String password, String name, String photo) async {
+    if (isemail(email) && validatePassword(password)) {
+      try {
+        User user = (await auth.createUserWithEmailAndPassword(
+                email: email, password: password))
+            .user!;
+
+        if (user != null) {
+          await DatabaseService(uid: user.uid)
+              .inituserdatawithphoto(user.uid, name, email, photo);
+          return user.getIdToken();
+        }
+      } on FirebaseAuthException catch (e) {
+        print(e);
+      }
+    } else {
+      throw "Password or email not of right type";
+    }
+    return null;
+  }
+
   Future withoutphotoregisterwithemailpassword(
       String email, String password, String name) async {
     if (isemail(email) && validatePassword(password)) {
@@ -74,6 +96,28 @@ class ChatAppPlugin {
     return false;
   }
 
+  Future<String?> tokenwithoutphotoregisterwithemailpassword(
+      String email, String password, String name) async {
+    if (isemail(email) && validatePassword(password)) {
+      try {
+        User user = (await auth.createUserWithEmailAndPassword(
+                email: email, password: password))
+            .user!;
+
+        if (user != null) {
+          await DatabaseService(uid: user.uid)
+              .inituserdatawithoutphoto(user.uid, name, email);
+          return user.getIdToken();
+        }
+      } on FirebaseAuthException catch (e) {
+        print(e);
+        // return e;
+      }
+    }
+
+    return null;
+  }
+
   Future customregister(
       String email, String profilephoto, String name, String uid) async {
     try {
@@ -82,6 +126,80 @@ class ChatAppPlugin {
     } on FirebaseException catch (e) {
       print(e);
     }
+  }
+
+  Future loginwithemailandpassword(String email, String password) async {
+    try {
+      User user = await auth.signInWithEmailAndPassword(
+          email: email, password: password) as User;
+      if (user != null) {
+        return true;
+      }
+      return true;
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+  }
+
+  Future<String?> tokenloginwithemailpassword(String email, password) async {
+    try {
+      User user = await auth.signInWithEmailAndPassword(
+          email: email, password: password) as User;
+      if (user != null) {
+        return user.getIdToken();
+      }
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+
+    return null;
+  }
+
+  Future<String?> tokenloginwithphonenumber(String phonenumber) async {
+    try {
+      User user = await auth.signInWithPhoneNumber(phonenumber) as User;
+      if (user != null) {
+        return user.refreshToken;
+      }
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+
+    return null;
+  }
+
+  Future loginwithphonenumber(String phonenumber) async {
+    try {
+      User user = await auth.signInWithPhoneNumber(phonenumber) as User;
+      if (user != null) {
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+
+    return false;
+  }
+
+  Future signout() async {
+    try {
+      await auth.signOut();
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+
+    return null;
+  }
+
+  Future sendforgotpassword(String email) async {
+    try {
+      auth.sendPasswordResetEmail(email: email);
+      return true;
+    } catch (e) {
+      print(e);
+    }
+
+    return false;
   }
   //get chat
   //add chat
